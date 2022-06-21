d3ac89d542d7e9aa646f708b97856d7b8e6a33c9
ChadPorkins <alderk40@gmail.com>
2022-06-21 15:42:39 +0300

    first of our code :)

---
+++ Suicide_attacks.R
@@ -0,0 +1,64 @@
+library(tidyverse)
+library(mapview)
+library(sf)
+library(ggplot2)
+library(ggmap)
+kaboom2 = read.csv("kaboom2.csv")
+kaboom2 = kaboom2[!is.na(kaboom2$latitude), ]
+kaboom2 = kaboom2[!is.na(kaboom2$longitude), ]
+kaboom3 = st_as_sf(kaboom2, coords = c("longitude", "latitude"),crs  = 4326)
+mapview(kaboom3)
+
+
+kaboom4 = ggplot(kaboom2, aes(x = longitude, y = latitude)) + 
+  geom_point() + 
+  coord_equal() + 
+  xlab('longitude') + 
+  ylab('latitude')
+
+
+
+ kaboom5 = ggplot(kaboom2, aes(x = longitude, y = latitude)) + 
+  geom_point(size = 0.1, alpha = 0.05) + 
+  coord_equal() + 
+  xlab('longitude') + 
+  ylab('latitude') + 
+  stat_density2d(aes(fill = ..level..), alpha = 100,
+                 geom = "polygon", data = kaboom2) + 
+  scale_fill_viridis_c() + 
+  theme(legend.position = 'none') 
+  
+ 
+   
+   
+   
+##################
+   world <- map_data("world")
+   ggplot() +
+     geom_map(
+       data = world, map = world,
+       aes(long, lat, map_id = region),
+       color = "black", fill = "lightgray", size = 0.1
+     ) +
+     geom_point(
+       data = kaboom2,
+       aes(longitude, latitude, color = "red2"),
+       alpha = 0.7
+     ) 
+   ######################3
+   ggplot(kaboom2, aes(x = longitude, y = latitude)) + 
+     geom_point() + 
+     coord_equal() + 
+     xlab('longitude') + 
+     ylab('latitude') + 
+     stat_density2d(aes(fill = ..level..), alpha = 100,
+                    geom = "polygon", data = kaboom2) + 
+     scale_fill_viridis_c() + 
+     theme(legend.position = 'none') 
+     
+   ##############################
+   ggplot(kaboom2, aes(x = longitude, y = latitude)) + 
+     geom_point() + 
+     coord_equal() + 
+     xlab('longitude') + 
+     ylab('latitude')
